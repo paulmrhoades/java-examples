7073c21046abd8425b99f418b61db5755c3d73e1
paulmrhoades <paulmrhoades@gmail.com>
2025-03-01 16:09:31 +0000

    Authentication & Authorization MVP

--- src/main/java/com/keepitsimples/examples/spring/security/controllers/HtmlController.java
+++ src/main/java/com/keepitsimples/examples/spring/security/controllers/HtmlController.java
@@ -19,69 +19,71 @@ public class HtmlController {
 
     private static final String RESOURCE_DIR = "src/main/resources/authorization/";
 
-    private final Log logger = LogFactory.getLog(HtmlController.class);
+    private static final Log logger = LogFactory.getLog(HtmlController.class);
 
     @GetMapping("/")
     public String home_page(Authentication authentication) {
         logger.info("'/' called");
-        return html("page_template.html", "Home Page", "This is the 'Home Page'", authentication);
+        return generate_html( "Home Page", "This is the 'Home Page'", authentication);
     }
 
     @GetMapping("/denied")
     public String denied_page(Authentication authentication) {
         logger.info("'/denied' called");
-        return html("page_template.html", "Access Denies", "You were DENIED access to this page.", authentication);
+        return generate_html("Access Denies", "You were DENIED access to this page.", authentication);
     }
 
     @GetMapping("/unprotected")
     public String unprotected_page(Authentication authentication) {
         logger.info("'/unprotected' called");
-        return html("page_template.html", "Unprotected Page", "You can access this page because it is NOT PROTECTED.", authentication);
+        return generate_html("Unprotected Page", "You can access this page because it is NOT PROTECTED.", authentication);
     }
 
     @GetMapping("/protected")
     public String protected_page(Authentication authentication) {
         logger.info("'/protected' called");
-        return html("page_template.html", "Protected Page", "You are ALLOWED to see this page because you are logged in.", authentication);
+        return generate_html( "Protected Page", "You are ALLOWED to see this page because you are logged in.", authentication);
     }
 
     @RolesAllowed("ADMIN")
     @GetMapping("/admin")
     public String admin_page(Authentication authentication) {
         logger.info("/admin' called");
-        return html("page_template.html", "Admin Page", "You are ALLOWED to see this page as you are logged in as a User with [Role=ADMIN]", authentication);
+        return generate_html("Admin Page", "You are ALLOWED to see this page as you are logged in as a User with [Role=ADMIN]", authentication);
     }
 
     @GetMapping("/login")
     public String login_page(Authentication authentication) {
         logger.info("'/login' called");
-        return html("page_template.html", "Login Page","Please login...", authentication);
+        return generate_html("Login Page","Please login...", authentication);
     }
 
-    private String html(String html_file, String title, String message, Authentication authentication) {
+    private String generate_html(String title, String message, Authentication authentication) {
         String html = "*** OOPS SOMETHING WENT WRONG ***";
         try {
-            final Path html_path = Paths.get(RESOURCE_DIR + html_file);
-            html = Files.readString(html_path);
-            html = html.replace("#TITLE#", title);
-            html = html.replace("#NAME#", name(authentication));
-            html = html.replace("#MESSAGE#", message);
-
             final Path css_path = Paths.get( RESOURCE_DIR + "styles.css");
             final String css = Files.readString(css_path);
+
+            final Path html_path = Paths.get(RESOURCE_DIR + "page_template.html");
+            html = Files.readString(html_path);
+            html = html.replace("#TITLE#", title);
             html = html.replace("#STYLES#", css);
+            html = html.replace("#MESSAGE#", message);
+            html = html.replace("#NAME#", name_and_authorities(authentication));
         } catch (Exception ioe) {
             ioe.printStackTrace();
         }
         return html;
     }
 
-    private boolean is_authenticated(Authentication authentication) {
-        return authentication != null && authentication.isAuthenticated();
-    }
-
-    private String name(Authentication authentication) {
-        return is_authenticated(authentication) ? "'" + authentication.getName() + "' " + authentication.getAuthorities() : "'Anonymous'" ;
+    private String name_and_authorities(Authentication authentication) {
+        final StringBuilder sb = new StringBuilder();
+        if (authentication != null && authentication.isAuthenticated()) {
+            sb.append("'").append(authentication.getName()).append("' ").append(authentication.getAuthorities());
+        } else {
+            sb.append("'Anonymous'");
+        }
+        return sb.toString();
     }
 
 }
